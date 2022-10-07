4c713acc50398cadd6b75056cf511dcda4c58fe5
Destinee Saunders <dsaunders290@gmail.com>
2022-10-07 16:00:16 -0400

    commit tree b9ed7d7528897265fdedc7406f6dc21e0a95e750

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+//element.append("h1")//
+newHeader.setAttribute("id","victory")
+newHeader.textContent="Destinee is the champion"
